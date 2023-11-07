241102e2fdc13e5b2361009fd73e753658bf5060
Martin Vidner <mvidner@suse.com>
2023-11-07 13:45:03 +0100

    dbus-translator-web (using Sinatra)

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "https://rubygems.org"
+ruby "3.2.2"
+
+gem "sinatra", "~> 2.2.0"
+gem "webrick", "~> 1.8"
--- lib/dbus-babel.rb
+++ lib/dbus-babel.rb
@@ -6,6 +6,21 @@ require_relative "dbus-babel/dbus-send"
 require_relative "dbus-babel/gdbus"
 
 module DBusBabel
+  def parse_argv(argv)
+    case argv.first
+    when "dbus-send"
+      DBusBabel::DBusSend.parse_argv(argv)
+    when "busctl"
+      DBusBabel::Busctl.parse_argv(argv)
+    when "gdbus"
+      DBusBabel::GDBus.parse_argv(argv)
+    else
+      warn "Unrecognized tool #{argv.first.inspect}"
+      nil
+    end
+  end
+  module_function :parse_argv
+
   class Message
     # @return [:signal,:method_call]
     attr_accessor :type
@@ -17,4 +32,3 @@ module DBusBabel
     attr_accessor :body
   end
 end
-
